0151c2820029739a70465a037f56cc70883b8490
Chris <cchu@8thlight.com>
2021-08-11 11:26:27 -0700

    Formatting

--- test/renderer_test.exs
+++ test/renderer_test.exs
@@ -1,33 +1,33 @@
 defmodule RendererTest do
   use ExUnit.Case
 
-	describe "handle_command tests: " do
-		test "quit outputs expected results string" do
-			expected = "Good bye"
+  describe "handle_command tests: " do
+    test "quit outputs expected results string" do
+      expected = "Good bye"
 
-			assert Renderer.handle_command("quit") == expected
-		end 
+      assert Renderer.handle_command("quit") == expected
+    end
 
-		test "start outputs expected results string" do
-			expected = """
-			Let's go
-			These are the following commands:
-			 1..9 - Choose a position matching one of these numbers
-			 quit - Quits Tic Tac Toe
-			"""
+    test "start outputs expected results string" do
+      expected = """
+      Let's go
+      These are the following commands:
+       1..9 - Choose a position matching one of these numbers
+       quit - Quits Tic Tac Toe
+      """
 
-			assert Renderer.handle_command("start") == expected
-		end
-		
-		test "begin outputs expected results string" do
-			expected = """
-			Welcome to Tic Tac Toe!
-			These are the following commands:
-			 start - Begin a game of Tic Tac Toe
-			 quit - Quits Tic Tac Toe
-			"""
-	
-			assert Renderer.handle_command("begin") == expected
-		end
-	end
+      assert Renderer.handle_command("start") == expected
+    end
+
+    test "begin outputs expected results string" do
+      expected = """
+      Welcome to Tic Tac Toe!
+      These are the following commands:
+       start - Begin a game of Tic Tac Toe
+       quit - Quits Tic Tac Toe
+      """
+
+      assert Renderer.handle_command("begin") == expected
+    end
+  end
 end
